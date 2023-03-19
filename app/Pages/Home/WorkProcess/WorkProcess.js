3c5caf185db80d56c549bc2ac06fac59db64b0b5
mdimtias <developertanbir@gmail.com>
2023-03-19 20:07:14 +0600

    Create Work Process Section

---
+++ app/Pages/Home/WorkProcess/WorkProcess.js
@@ -0,0 +1,64 @@
+const WorkProcess = () => {
+    return (
+        <section className="py-10 lg:py-20 bg-white">
+            <div className="container mx-auto px-5">
+            <div className="section-heading text-center">
+          <h4 className="text-base text-[#175CFF] font-bold mb-3">
+            Process
+          </h4>
+          <h2 className="text-4xl font-bold text-black mb-3">
+          We Follow Our Work Process
+          </h2>
+         <d className="flex justify-center items-center">
+         <p className="text-[#737373] text-base mb-3 lg:w-[60%]">
+          Conveniently mesh cooperative services via magnetic outsourcing. Dynamically grow functionalized value whereas accurate e-commerce.
+          </p>
+         </d>
+        </div>
+        <div className="work-process grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 py-7">
+        <div className="work-plan px-8 py-12 flex justify-center items-center flex-col group hover:shadow-2xl relative">
+                <div className="one p-5 border w-16 h-16 rounded-lg flex justify-center items-center border-gray-400 group-hover:bg-[#175CFF] text-[#175CFF] group-hover:text-white mb-4">
+                    <h3 className="font-bold hover:text-white text-2xl">1</h3>
+                </div>
+                <div className="work-plan-content">
+                    <h2 className="font-bold text-black text-xl text-center mb-3">Research</h2>
+                    <p className="text-[#737373] text-base text-center">Uniquely for compelling initiatives expanded interactive.</p>
+                </div>
+                <div className="dot-line hidden lg:block w-20 border-dashed border-[1px] border-[#175CFF] absolute -right-12 group-hover:-z-10"></div>
+            </div>
+            <div className="work-plan px-8 py-12 flex justify-center items-center flex-col group hover:shadow-2xl relative">
+                <div className="one p-5 border w-16 h-16 rounded-lg flex justify-center items-center border-gray-400 group-hover:bg-[#175CFF] text-[#175CFF] group-hover:text-white mb-4">
+                    <h3 className="font-bold hover:text-white text-2xl">2</h3>
+                </div>
+                <div className="work-plan-content">
+                    <h2 className="font-bold text-black text-xl text-center mb-3">Designing</h2>
+                    <p className="text-[#737373] text-base text-center">Restore efficient human pursue for compelling initiatives.</p>
+                </div>
+                <div className="dot-line hidden lg:block  w-20 border-dashed border-[1px] border-[#175CFF] absolute -right-12 group-hover:-z-10"></div>
+            </div>
+            <div className="work-plan px-8 py-12 flex justify-center items-center flex-col group hover:shadow-2xl relative">
+                <div className="one p-5 border w-16 h-16 rounded-lg flex justify-center items-center border-gray-400 group-hover:bg-[#175CFF] text-[#175CFF] group-hover:text-white mb-4">
+                    <h3 className="font-bold hover:text-white text-2xl">3</h3>
+                </div>
+                <div className="work-plan-content">
+                    <h2 className="font-bold text-black text-xl text-center mb-3">Building</h2>
+                    <p className="text-[#737373] text-base text-center">Continually enhance pursue compelling initiatives enhance.</p>
+                </div>
+                <div className="dot-line hidden lg:block  w-20 border-dashed border-[1px] border-[#175CFF] absolute -right-12 group-hover:-z-10"></div>
+            </div>
+            <div className="work-plan px-8 py-12 flex justify-center items-center flex-col group hover:shadow-2xl relative">
+                <div className="one p-5 border w-16 h-16 rounded-lg flex justify-center items-center border-gray-400 group-hover:bg-[#175CFF] text-[#175CFF] group-hover:text-white mb-4">
+                    <h3 className="font-bold hover:text-white text-2xl">4</h3>
+                </div>
+                <div className="work-plan-content">
+                    <h2 className="font-bold text-black text-xl text-center mb-3">Deliver</h2>
+                    <p className="text-[#737373] text-base text-center">Uniquely for compelling initiatives expanded interactive.</p>
+                </div>
+            </div>
+        </div>
+            </div>
+        </section>
+    );
+};
+
+export default WorkProcess;
--- app/page.js
+++ app/page.js
@@ -3,15 +3,17 @@ import Integration from "./Pages/Home/Integration/Integration";
 import LatestNews from "./Pages/Home/LatestNews/LatestNews";
 import Subscribe from "./Pages/Home/Subscribe/Subscribe";
 import Testimonial from "./Pages/Home/Testimonial/Testimonial";
+import WorkProcess from "./Pages/Home/WorkProcess/WorkProcess";
 
 export default function Home() {
   return (
     <main className="">
         {/* <Hero></Hero>
         <Subscribe></Subscribe>
-        <Integration></Integration> */}
+        <Integration></Integration>
         <LatestNews></LatestNews>
-        <Testimonial></Testimonial>
+        <Testimonial></Testimonial> */}
+        <WorkProcess></WorkProcess>
     </main>
   )
 }
