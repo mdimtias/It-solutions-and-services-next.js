cb1045cf2c922cf504dffdb70d66ff95db10a995
mdimtias <developertanbir@gmail.com>
2023-03-19 21:57:22 +0600

    Create Report Section

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import Hero from "./Pages/Home/Hero/Hero";
 import Integration from "./Pages/Home/Integration/Integration";
 import LatestNews from "./Pages/Home/LatestNews/LatestNews";
+import Reports from "./Pages/Home/Reports/Reports";
 import Subscribe from "./Pages/Home/Subscribe/Subscribe";
 import Testimonial from "./Pages/Home/Testimonial/Testimonial";
 import WorkProcess from "./Pages/Home/WorkProcess/WorkProcess";
@@ -13,7 +14,8 @@ export default function Home() {
         <Integration></Integration>
         <LatestNews></LatestNews>
         <Testimonial></Testimonial> */}
-        <WorkProcess></WorkProcess>
+        {/* <WorkProcess></WorkProcess> */}
+        <Reports></Reports>
     </main>
   )
 }
